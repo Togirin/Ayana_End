0554178557d5a43d8048b5addb49a5c1ab9d7c7b
Togirin <51913996+Togirin@users.noreply.github.com>
2019-08-11 18:53:10 +0900

    Update ayana_end.py

--- ayana_end.py
+++ ayana_end.py
@@ -65,7 +65,6 @@ async def on_message(message):
         await app.send_message(message.channel, "조용히 하고 방송이나 보러가랏, https://www.youtube.com/channel/UC7uKMg7lPdCp4Nm1tLesihQ")
     if message.content == "^명령어":
         embed = discord.Embed(title="명령어 목록", description="명령어 목록입니닷!", colour = discord.Colour.blue())
-
         #embed.set_footer(text = "추가명령어는 토끼린에게 문의바람")
         dtime = datetime.datetime.now()
         #print(dtime[0:4]) # 년도
@@ -91,6 +90,7 @@ async def on_message(message):
 
     if message.content.startswith("ㅇㅅㅇ"):
         await app.send_message(message.channel, "ㅇㅅㅇ")
+        
     if message.content.startswith("^주홍정체"):
         await app.send_message(message.channel, "53사단 125연대 3대대 10중대 일병 주홍"
 
@@ -107,56 +107,6 @@ async def on_message(message):
         choicenumber = random.randint(1, len(choice)-1)
         choiceresult = choice[choicenumber]
         await app.send_message(message.channel, choiceresult)
-    
-    if message.content.startswith("^롤"):
-        learn = message.content.split(" ")
-        location = learn[1]
-        enc_location = urllib.parse.quote(location)
-
-        url = "https://www.op.gg/summoner/userName=" + enc_location
-        html = urllib.request.urlopen(url)
-
-        bsObj = bs4.BeautifulSoup(html, "html.parser")
-        rank1 = bsObj.find("div", {"class": "TierRankInfo"})
-        rank2 = rank1.find("div", {"class": "TierRank"})
-        rank3 = rank2.find("span", {"class": "tierRank"})
-        rank4 = rank3.text  # 티어표시 (브론즈1,2,3,4,5 등등)
-        print(rank4)
-        if rank4 != 'Unranked':
-          jumsu1 = rank1.find("div", {"class": "TierInfo"})
-          jumsu2 = jumsu1.find("span", {"class": "LeaguePoints"})
-          jumsu3 = jumsu2.text
-          jumsu4 = jumsu3.strip()#점수표시 (11LP등등)
-          print(jumsu4)
-
-          winlose1 = jumsu1.find("span", {"class": "WinLose"})
-          winlose2 = winlose1.find("span", {"class": "wins"})
-          winlose2_1 = winlose1.find("span", {"class": "losses"})
-          winlose2_2 = winlose1.find("span", {"class": "winratio"})
-
-          winlose2txt = winlose2.text
-          winlose2_1txt = winlose2_1.text
-          winlose2_2txt = winlose2_2.text #승,패,승률 나타냄  200W 150L Win Ratio 55% 등등
-
-          print(winlose2txt + " " + winlose2_1txt + " " + winlose2_2txt)
-
-        channel = message.channel
-        embed = discord.Embed(
-            title='롤 정보',
-            description='롤 정보입니다.',
-            colour=discord.Colour.green()
-        )
-        if rank4=='Unranked':
-            embed.add_field(name='당신의 티어', value=rank4, inline=False)
-            embed.add_field(name='-당신은 언랭-', value="언랭은 더이상의 정보는 제공하지 않습니다.", inline=False)
-            await app.send_message(channel, embed=embed)
-        else:
-         embed.add_field(name='당신의 티어', value=rank4, inline=False)
-         embed.add_field(name='당신의 LP(점수)', value=jumsu4, inline=False)
-         embed.add_field(name='당신의 승,패 정보', value=winlose2txt+" "+winlose2_1txt, inline=False)
-         embed.add_field(name='당신의 승률', value=winlose2_2txt, inline=False)
-         await app.send_message(channel, embed=embed)
-
 
     if message.content.startswith('^고양이'):
         embed = discord.Embed(
